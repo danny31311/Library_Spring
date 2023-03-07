920d0ae4fac72a66145f6e4a7cb4bc9385db42c7
danny31311 <stebliy2@gmail.com>
2023-03-07 19:24:18 +0300

    Updated PersonValidator

--- src/main/java/org/example/util/PersonValidator.java
+++ src/main/java/org/example/util/PersonValidator.java
@@ -1,5 +1,34 @@
 package org.example.util;
 
-public class PersonValidator {
+import org.example.dao.PersonDAO;
+import org.example.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+@Component
+public class PersonValidator implements Validator {
 
+    private final PersonDAO personDAO;
+    @Autowired
+    public PersonValidator(PersonDAO personDAO){
+        this.personDAO = personDAO;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz );
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        if(personDAO.show(person.getName()).isPresent()){
+            errors.rejectValue("name", "","This name is already taken");
+        }
+
+        //Посмотреть, есть ли человек с таким же имененм в БД
+
+    }
 }
