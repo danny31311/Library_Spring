c8f795b7bd76d213a794f1dc8da61c5860deab16
danny31311 <stebliy2@gmail.com>
2023-03-07 15:11:01 +0300

    Updated Person model

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,4 +1,43 @@
 package org.example.models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Person {
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int id;
+    @NotEmpty(message = "Name can not be empty")
+    private String name;
+
+    @Min(value = 0, message = "I absolutely sure you are older than 0")
+    private int age;
 }
