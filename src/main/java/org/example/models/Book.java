8819c31390d868e19b827a1afcf9573ef506ea74
danny31311 <stebliy2@gmail.com>
2023-03-07 18:02:32 +0300

    Updated Book model #2

--- src/main/java/org/example/models/Book.java
+++ src/main/java/org/example/models/Book.java
@@ -4,6 +4,45 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
 public class Book {
+    public Book(int id, String book_name, String author, int year) {
+        this.id = id;
+        this.book_name = book_name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     private int id;
     @NotEmpty(message = "Book name can not be empty")
     private String book_name;
