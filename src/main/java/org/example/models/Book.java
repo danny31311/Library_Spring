39c912641d9e5577580e2188d8d63b6f8b1b399d
danny31311 <stebliy2@gmail.com>
2023-03-07 18:01:03 +0300

    Updated Book model

--- src/main/java/org/example/models/Book.java
+++ src/main/java/org/example/models/Book.java
@@ -1,4 +1,15 @@
 package org.example.models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Book {
+    private int id;
+    @NotEmpty(message = "Book name can not be empty")
+    private String book_name;
+    @NotEmpty(message = "String author can not be empty")
+    private String author;
+    @Min(value = 0, message = "I doubt you want add book which was written B.C. ")
+    private int year;
+
 }
